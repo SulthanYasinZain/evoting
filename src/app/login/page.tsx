af30d0cd16b0d0230c0263e6f3557e37ce3565dd
SulthanYasin <sulthanzain28@gmail.com>
2025-05-25 17:53:58 +0700

    refactor: remove candidate and election dialog components
    - Deleted `deletecandidateDialog.tsx`, `editcandidateDialog.tsx`, and `editelectionDialog.tsx` components to streamline the codebase.
    - Removed associated UI components such as `electionCard.tsx`, `electionStatus.tsx`, and `electiondetailCard.tsx`.
    - Eliminated image upload functionality by removing `imageUpload.tsx`.
    - Cleaned up unused loading and login components: `loadingState.tsx`, `loginForm.tsx`, and `logoutButton.tsx`.
    - Removed navbar component and related logic for user authentication.
    - Deleted utility functions and types including `formatToInputDate.ts` and `utils.ts`.
    - Cleaned up the `tsconfig.json` file to remove unnecessary configurations.

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/loginForm";
-
-export default async function Page() {
-  return (
-    <section className="flex justify-center items-center h-[89svh]">
-      <LoginForm />
-    </section>
-  );
-}
